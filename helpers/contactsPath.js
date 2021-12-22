346b3ca4fcbd8aafabd0b84f07edd829ca2e0d3f
Andrey Bulanov <bulanov1804@gmail.com>
2021-12-22 22:25:33 +0200

    Убрал лишние папки

--- helpers/contactsPath.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-const contactsPath = path.resolve(__dirname, '../', 'db', 'contacts.json')
-
-module.exports = contactsPath;
--- schemas/contactsSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const contactsSchema = Joi.object({
-    name: Joi.string().min(2).max(40).required(),
-    email: Joi.string().min(4).max(40).required(),
-    phone: Joi.string().min(8).max(20).required(),
-});
- 
-module.exports = { contactsSchema };
